0af5e0e8045c763385039b4d0b3aab6f937ab247
Phoenix <aja.magdaleno@gmail.com>
2018-10-23 20:22:42 -0700

    jsxRoutes done

--- routes/jsxRoutes.js
+++ routes/jsxRoutes.js
@@ -5,7 +5,14 @@ var restaurant = function(app) {
     app.get("/", (req,res) => {
         res.render("index")
     })
+
     app.get("/res_view", (req,res) => {
         res.render("/components/res_view")
     })
+
+    app.get("/reservation", (req,res) => {
+        res.render("/components/reservation")
+    })
 }
+
+module.exports = restaurant
