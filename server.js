6c5168b6d13849cc7ba46b343ca858ac2c55acf7
pensadotech <armando@pensadotech.com>
2018-10-23 20:14:14 -0700

    add react folder for views

--- server.js
+++ server.js
@@ -4,6 +4,11 @@ const bodyParser = require('body-parser')
 const path = require('path')
 const app = express()
 
+// JSX 
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jsx');
+app.engine('jsx', require('express-react-views').createEngine());
+
 // express middelware: defines static file directory
 app.use(express.static(path.join(__dirname, 'app/public')))
 // express middleware: capable to handle complex json
